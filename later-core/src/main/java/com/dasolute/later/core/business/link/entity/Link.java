b53abcfafb7b7b85cae9639578ac5484eaa56c61
chopokmado <chopokmado@gmail.com>
2020-09-16 00:15:13 +0900

    초기 gradle multi 프로젝트 세팅

---
+++ later-core/src/main/java/com/dasolute/later/core/business/link/entity/Link.java
@@ -0,0 +1,13 @@
+package com.dasolute.later.core.business.link.entity;
+
+
+import javax.persistence.Column;
+
+public class Link {
+
+	@Column
+	private Long id;
+
+	@Column
+	private String address;
+}
