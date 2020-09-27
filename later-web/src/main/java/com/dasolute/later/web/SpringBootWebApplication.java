8aea12788f6dcc44628f963c08462bf3927da5e4
chopokmado <chopokmado@gmail.com>
2020-09-27 22:27:59 +0900

    later-1 간단한 프리마커 템플릿 적용해보기  - 정말 간단한 프리마커 템플릿 적용

--- later-web/src/main/java/com/dasolute/later/web/SpringBootWebApplication.java
+++ later-web/src/main/java/com/dasolute/later/web/SpringBootWebApplication.java
@@ -2,19 +2,13 @@ package com.dasolute.later.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.AbstractEnvironment;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.util.Arrays;
 
-/**
- * Created by simons on 2017. 4. 20..
- */
-@SpringBootApplication(scanBasePackages = "com.dasolute.later")
-public class SpringBootWebApplication extends SpringBootServletInitializer {
+@SpringBootApplication
+public class SpringBootWebApplication {
 
     public static void main(String args[]) {
         System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "local");
@@ -22,12 +16,4 @@ public class SpringBootWebApplication extends SpringBootServletInitializer {
         Arrays.stream(ca.getBeanDefinitionNames()).forEach(System.out::println);
     }
 
-    /**
-     * 외부 톰캣에서 war 파일을 띄우기 위해 필요한 Class 및 Override 해야할 method
-     * SpringBootServletInitializer
-     */
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(SpringBootWebApplication.class);
-    }
 }
